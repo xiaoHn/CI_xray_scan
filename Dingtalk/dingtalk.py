a3f4cdca1639bdc4e5fee7e23a87910bd2cd70af
xiaoHn <973974776@qq.com>
2020-07-15 16:38:26 +0800

    Update dingtalk.py

--- Dingtalk/dingtalk.py
+++ Dingtalk/dingtalk.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 def dingtalk(error_title,error_text,apiid):
-    url = 'https://oapi.dingtalk.com/robot/send?access_token=7b40f1ab81d14671f5878a58f69a64b6e1d254f0207d6c9ede4f3c5cd4e10aaa'
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=Youtoken'
     headers = {
         'cache-control': 'no-cache',
         'Content-type': 'application/json',
@@ -14,8 +14,8 @@ def dingtalk(error_title,error_text,apiid):
         "link": {
             "text": error_text,
             "title": error_title,
-            "picUrl": "http://static.seeyouyima.com/www.meiyou.com/meiyou-bf23e296a9058a8dd5581eda3ea59674.png",
-            "messageUrl": "https://apidoc.seeyouyima.com/doc/" + str(apiid)
+            "picUrl": "http://static.test.com/www.mtestu.com/test-bf23e296a9058a8dd5581eda3ea59674.png",
+            "messageUrl": "https://apidoc.test.com/doc/" + str(apiid)
         }
     }
     try:
