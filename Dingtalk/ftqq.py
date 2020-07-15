48e6372c2d25e7cc56a987b3a208e9e1ce696ebd
xiaoHn <973974776@qq.com>
2020-07-15 16:38:46 +0800

    Update ftqq.py

--- Dingtalk/ftqq.py
+++ Dingtalk/ftqq.py
@@ -4,10 +4,10 @@ import requests
 
 
 def push_ftqq(content):
-    resp = requests.post("https://sc.ftqq.com/SCU69492Te3ed88766d48ccec1912edccfe2537b35df9f19ae62ad.send",data={"text": "vuls notice", "desp": content})
+    resp = requests.post("https://sc.ftqq.com/Youtoken.send",data={"text": "vuls notice", "desp": content})
     if resp.json()["errno"] != 0:
         raise ValueError("push ftqq failed, %s" % resp.text)
 def push_ftqqss(content):
-    resp = requests.post("https://pushbear.ftqq.com/sub",data={"sendkey": "16747-a420c7eb8a0f24f42ea49baed26326b1","text": "vuls notice", "desp": content})
+    resp = requests.post("https://pushbear.ftqq.com/sub",data={"sendkey": "16747-Youtoken","text": "vuls notice", "desp": content})
     #if resp.json()["errno"] != 0:
         #raise ValueError("push ftqq failed, %s" % resp.text)
